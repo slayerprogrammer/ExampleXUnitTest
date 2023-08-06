8c550e7aa6ba0aba97c9aa97bec328708d16f679
tolgahan.ozturk <tolgahan.ozturk@saglik.gov.tr>
2023-08-06 18:25:02 +0300

    https://tolgahanozturk.medium.com/net-core-i%CC%87le-unit-test-2-9d6c5cd69c66

--- ExampleUnitTest.APP/Calculator.cs
+++ ExampleUnitTest.APP/Calculator.cs
@@ -10,6 +10,9 @@ namespace ExampleUnitTest.APP
     {
         public int add(int a, int b)
         {
+            if(a == 0 || b==0)
+                return 0;
+
             return a + b;
         }
     }
